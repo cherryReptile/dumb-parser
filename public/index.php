fb60529cca32fbb5a7bfcc6ad0579f8ca375d62a
cherryReptile <lisovskiy.nikitk@gmail.com>
2022-08-28 18:21:46 +0300

    sorting

--- public/index.php
+++ public/index.php
@@ -1,15 +1,26 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../src/DumbParser.php';
 
-use Receiver\DumbParser;
+$arr = [4, 5, 8, 9, 1, 7, 2];
 
-try {
-    $receiver = new DumbParser("https://example.com");
-    $result = $receiver->tagsWithCount();
-
-    dd($result);
-} catch (Error $error) {
-    dd($error);
+function sortArr(array $arr) : array{
+    for ($count = count($arr); $count > 1; $count--) {
+        for ($i = 1; $i < $count; $i++) {
+            if ($arr[$i] > $arr[0]) {
+                array_swap($arr, $i);
+            }
+            array_swap($arr, $count - 1);
+        }
+    }
+    return $arr;
 }
+
+function array_swap(&$arr, $num)
+{
+    $first = $arr[0];
+    $arr[0] = $arr[$num];
+    $arr[$num] = $first;
+}
+
+dd(sortArr($arr));
